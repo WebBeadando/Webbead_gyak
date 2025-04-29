410db9587e23018a45d50dafb8eca3aa9d2683a7
KLNK5L-egyik <61226397+balintx3941@users.noreply.github.com>
2025-04-29 14:46:52 +0200

    1-3
    Első feladat átalakítása, második és harmadik feladat elkészítése

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'site_title' => 'Teszt',
+    'menu' => [
+        'home' => 'Home',
+        'about' => 'About Us',
+        'contact' => 'Contact'
+    ]
+];
+?>
---
+++ pages/contact.php
@@ -0,0 +1,2 @@
+<h2>Contact Us</h2>
+<p>You can reach us at contact@example.com.</p>
