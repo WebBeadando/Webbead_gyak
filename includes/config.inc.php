050e2c02a96b8322cc9bbd5b0a6a4e15e20606f1
KLNK5L-egyik <61226397+balintx3941@users.noreply.github.com>
2025-04-30 13:39:09 +0200

    4.feladat
    Weblap átdolgozása a belépés ,kilépés és regisztráció miatt.

---
+++ includes/config.inc.php
@@ -0,0 +1,30 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Minta oldal.',
+);
+
+$fejlec = array(
+    'kepforras' => '',
+    'kepalt' => '',
+	'cim' => 'Minta oldal',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Minta Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'uzenet' => array('fajl' => 'uzenet', 'szoveg' => 'Üzenet', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
